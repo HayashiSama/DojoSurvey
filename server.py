576b97756c6583e43f52f0c15a66034d77c0f7c5
Peter Lin <ptrlin@ucdavis.edu>
2018-02-07 14:00:27 -0800

    color change adding

--- server.py
+++ server.py
@@ -19,6 +19,15 @@ def ninjas():
 	
   	return render_template("results.html", name=name, location=location, language=language, comments=comments)
 
-
+@app.route('/color', methods=['POST'])
+def colorify():
+	red=int(request.form['red'])
+	green=int(request.form['green'])
+	blue=int(request.form['blue'])
+	if(red >= 0 and red <= 255 and green >= 0 and green <= 255 and blue >= 0 and blue <= 255):
+		print "Color in bounds"
+		return render_template("index.html", red=red, green=green, blue=blue)
+	else:
+		return render_template("index.html", error=True)
 
 app.run(debug=True) # run our server
