4fd8e267c2fc482a5d735cfee49bbadec634c7e5
Peter Lin <ptrlin@ucdavis.edu>
2018-02-08 16:48:43 -0800

    adding validations

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+app.secret_key = '0118999881999119725'
+
+
 # our index route will handle rendering our form
 @app.route('/')
 def index():
@@ -12,10 +18,22 @@ def ninjas():
 	location = request.form['location']
 	language = request.form['language']
 	comments=request.form['comments']
-	print name
-	print location
-	print language
-
+	email=request.form['email']
+	if(len(name)==0):
+		flash("Name cannot be empty!")
+		return redirect('/')
+	if(len(email) < 1):
+		flash("Email cannot be empty!")
+		return redirect('/')
+	elif not EMAIL_REGEX.match(email):
+		flash("Invalid Email Address!")	
+		return redirect('/')
+	if(len(comments)>140):
+		flash("Comments must be under 140 chars")	
+		return redirect('/')
+	if(len(comments)<1):
+		flash("Comments cannot be blank")	
+		return redirect('/')
 	
   	return render_template("results.html", name=name, location=location, language=language, comments=comments)
 
